6c98538dac86812c79fd5bb08751fafcf2af9e8b
Frederick Jacob <128831345+Quewo@users.noreply.github.com>
2024-06-20 18:16:03 +0000

    Second Copilot Commit

--- member.js
+++ member.js
@@ -1,3 +1,8 @@
 function skillsMember() {
-    
+    // Add your code here
+    // Define an array of skills for the member
+    const skills = ['JavaScript', 'HTML', 'CSS', 'React'];
+
+    // Return the array of skills
+    return skills;
 }
